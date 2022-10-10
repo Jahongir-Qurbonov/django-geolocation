9708e7050f5d442b1c28805c663127bad7150471
Jahongir Qurbonov <qurbonovjahongir17@gmail.com>
2022-10-10 10:19:17 +0500

    set verbose name measurements

--- measurements/apps.py
+++ measurements/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class MeasurementsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'measurements'
+    verbose_name = _('Measurement beetwen 2 locations')
